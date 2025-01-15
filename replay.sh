8a3040c1027583edf46a10f38d25548606490840
Gustavo Marques <gmarques@ucar.edu>
2025-01-15 14:58:38 -0700

    local git repository created

---
+++ replay.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Created 2025-01-15 14:57:17
+
+CASEDIR="/glade/work/gmarques/cesm.cases/G/g.e30_a05cish.GJRAv4.TL319_t232_wgx3_hycom1_N75.2025.009"
+
+/glade/work/gmarques/cesm.sandboxes/cesm3_0_alpha05c/cime/scripts/create_newcase --case g.e30_a05cish.GJRAv4.TL319_t232_wgx3_hycom1_N75.2025.009 --compset 2000_DATM%JRA-1p4-2018_SLND_CICE_MOM6_DROF%JRA-1p4-2018_SGLC_WW3_SESP --res TL319_t232_wg37 --run-unsupported
+
+cd "${CASEDIR}"
+
+./pelayout
+
+./xmlchange NTASKS_OCN=896
+
+./case.setup
+
